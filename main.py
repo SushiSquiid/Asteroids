9086e4d35ba615aeaef6b796aad9fe10322986c6
SushiSquiid <charlie.a.griffiths@gmail.com>
2024-12-31 02:50:01 +0000

    game now has a functioning player object

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
@@ -14,7 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
